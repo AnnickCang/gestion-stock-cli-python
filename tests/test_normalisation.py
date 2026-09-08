8a1ee9844fe9146c7eef43570f5fb6996aacf2eb
Annick Cang <annick.cang@gmail.com>
2026-09-08 21:27:36 +0200

    test(normalisation): add multiple cases to normalization test

--- tests/test_normalisation.py
+++ tests/test_normalisation.py
@@ -1,10 +1,24 @@
+import pytest
+
 from normalisation import normaliser_chaine_pour_comparaison
 
 
-def test_normaliser_chaine_pour_comparaison():
-    chaine_entree = "Café"
-    chaine_attendue = "cafe"
+@pytest.mark.parametrize(
+    ("chaine_entree", "chaine_attendue"),
+    [
+        ("Café", "cafe"),
+        ("caramel", "caramel"),
+        ("décaféiné", "decafeine"),
+        ("ÀÂÉÈÊÙÛÔÎ", "aaeeeuuoi"),
+        ("m&ms", "m&ms"),
+        ("Niño", "nino"),
+        ("<>*/- @&", "<>*/- @&"),
+        ("", ""),
+    ],
+)
 
+
+def test_normaliser_chaine_pour_comparaison(chaine_entree, chaine_attendue):
     chaine_sortie = normaliser_chaine_pour_comparaison(chaine_entree)
 
     assert chaine_sortie == chaine_attendue
